3bbb798fcc948274b6ee9d19356074348a5095d3
Zeezbaba <Azeez_okeleji>
2024-06-13 15:19:42 +0000

    Advanced mysql Task: 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,17 +3,24 @@
 -- An average score can be a decimal
 -- Procedure ComputeAverageScoreForUser is taking 1 input:
 -- user_id, a users.id value (you can assume user_id is linked to an existing users)
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-        DECLARE avg_score FLOAT;
-        SELECT AVG(score) INTO avg_score
+        DECLARE score_sum INT DEFAULT 0;
+        DECLARE project_count INT DEFAULT 0;
+
+        SELECT SUM(score) INTO score_sum
         FROM corrections
-        WHERE user_id = user_id;
+        WHERE corrections.user_id = user_id;
+
+        SELECT COUNT(*) INTO project_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
 
         UPDATE users
-        SET average_score = avg_score
-        WHERE id = user_id;
+        SET users.average_score = IF(project_count = 0, 0, score_sum / project_count)
+        WHERE users.id = user_id;
 END //
 DELIMITER ;
