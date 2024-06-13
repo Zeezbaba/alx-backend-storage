f23f5e0cb2ea0891cb22bb9b5678fb55c1d4fc22
Zeezbaba <Azeez_okeleji>
2024-06-13 13:12:39 +0000

    Advanced mysql Task: 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,8 +6,8 @@ CREATE TRIGGER reset_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-        IF NEW.email != OLD.email THEN
+        IF NEW.email <> OLD.email THEN
                 SET NEW.valid_email = 0;
         END IF;
 END //
-DELIMETER;
+DELIMETER ;
