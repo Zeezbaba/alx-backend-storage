8890872c4aad70511b1092e1a64c44faaf3547f9
Zeezbaba <Azeez_okeleji>
2024-06-18 23:45:31 +0000

    NoSQL Task: 11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""a Python function that returns the list of school having a specific topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Returns list of schools having a specific topic"""
+    query = { "topics": topic }
+    return list(mongo_collection.find(query))
