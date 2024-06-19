f9bf2e776c311e896635e236de98d5a1123a7065
Zeezbaba <Azeez_okeleji>
2024-06-19 00:32:55 +0000

    NoSQL Task: 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""a Python script that provides some stats
+about Nginx logs stored in MongoDB
+"""
+
+from pymongo import MongoClient
+
+
+def count_nginx_logs(mongo_collection):
+    """provide stats about Nginx logs stored in MongoDB"""
+    print('{} logs'.format(mongo_collection.count_documents({})))
+
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        query = {"method": method}
+        count = mongo_collection.count_documents(query)
+        print(f"\tmethod {method}: {count}")
+
+    status_count = mongo_collection.count_documents(
+            {"method": "GET", "path": "/status"})
+    print(f"{status_count} status check")
+
+
+if __name__ == "__main__":
+    mongo_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    count_nginx_logs(mongo_collection)
