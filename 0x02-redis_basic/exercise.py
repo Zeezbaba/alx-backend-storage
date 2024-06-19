fbe5cf16f10acd119b4449b71d6a2e726b0a5c9a
Zeezbaba <Azeez_okeleji>
2024-06-19 22:02:30 +0000

    redis task: 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -25,3 +25,23 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
+        """take string and optional callable as argument"""
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """parametrize Cache.get"""
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> Optional[int]:
+        """parametrize Cache.get"""
+        value = self.get(key, fn=int)
+        return value
