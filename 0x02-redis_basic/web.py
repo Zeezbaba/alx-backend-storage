aed306e2942420124540787c86bdcb09964c7b6e
Zeezbaba <Azeez_okeleji>
2024-06-20 23:54:23 +0000

    redis task: 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,30 +9,37 @@ from functools import wraps
 
 r = redis.Redis()
 
-def track_access_count(method):
-    """Decorator to track the number of times a URL is accessed"""
+
+def url_access_count(method):
+    """Decorator for get_page function"""
     @wraps(method)
     def wrapper(url):
-        """wrapper function"""
+        """Wrapper function"""
         key = "cached:" + url
         cached_value = r.get(key)
         if cached_value:
             return cached_value.decode("utf-8")
-        
+
         # Get new content and update cache
         key_count = "count:" + url
         html_content = method(url)
-        
+
         r.incr(key_count)
-        r.set(key, html_content, ex=10)
+        r.setex(key, 10, html_content)
         return html_content
     return wrapper
 
-@track_access_count
+
+@url_access_count
 def get_page(url: str) -> str:
-    """Fetch the HTML content of a particular URL"""
-    response = requests.get(url)
-    return response.text
+    """Obtain the HTML content of a particular URL"""
+    try:
+        results = requests.get(url)
+        results.raise_for_status()  # Raise an HTTPError for bad responses
+        return results.text
+    except requests.RequestException as e:
+        return f"Error: {e}"
+
 
 if __name__ == "__main__":
     print(get_page('http://slowwly.robertomurray.co.uk'))
