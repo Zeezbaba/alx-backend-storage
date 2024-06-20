82136342791e889fc18e2f94229f734bbe2abd2d
Zeezbaba <Azeez_okeleji>
2024-06-20 23:46:02 +0000

    redis task: 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,11 +5,9 @@ HTML content of a particular URL and returns it.
 
 import redis
 import requests
-from typing import Callable
 from functools import wraps
 
 r = redis.Redis()
-
 
 def track_access_count(method):
     """Decorator to track the number of times a URL is accessed"""
@@ -20,17 +18,15 @@ def track_access_count(method):
         cached_value = r.get(key)
         if cached_value:
             return cached_value.decode("utf-8")
-
-            # Get new content and update cache
+        
+        # Get new content and update cache
         key_count = "count:" + url
         html_content = method(url)
-
+        
         r.incr(key_count)
         r.set(key, html_content, ex=10)
-        r.expire(key, 10)
         return html_content
     return wrapper
-
 
 @track_access_count
 def get_page(url: str) -> str:
@@ -38,6 +34,5 @@ def get_page(url: str) -> str:
     response = requests.get(url)
     return response.text
 
-
 if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    print(get_page('http://slowwly.robertomurray.co.uk'))
